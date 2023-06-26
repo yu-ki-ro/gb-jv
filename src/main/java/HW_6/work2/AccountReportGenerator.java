bac71e44c8a9e1d41d8faf6f305508c88d347a9a
yukiro <yudanovk@yandex.ru>
2023-06-26 18:18:53 +0300

    gb

---
+++ src/main/java/HW_6/work2/AccountReportGenerator.java
@@ -0,0 +1,10 @@
+package HW_6.work2;
+
+public class AccountReportGenerator implements ReportGeneratorJson {
+
+    @Override
+    public String generateJson() {
+        String report = "";
+        return report;
+    }
+}
---
+++ src/main/java/HW_6/work2/DocumentReportGenerator.java
@@ -0,0 +1,16 @@
+package HW_6.work2;
+
+public class DocumentReportGenerator implements ReportGeneratorXml, ReportGeneratorJson {
+
+    @Override
+    public String generateXml() {
+        String report = "";
+        return report;
+    }
+
+    @Override
+    public String generateJson() {
+        String report = "";
+        return report;
+    }
+}
